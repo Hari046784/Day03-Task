fb48e151ecf4a482be550a1955abce517b52bc0d
HaridassMurugan <108067743+HaridassMurugan@users.noreply.github.com>
2022-09-23 16:01:41 +0530

    Add files via upload

---
+++ Day 3 Task resume.js
@@ -0,0 +1,57 @@
+let myResume={
+    "basics": {
+      "name": "HARIDASS MURUGAN",
+      "email": "Haridass046@gamil.com",
+      "phone": 9003088540,
+      "degree": "B.E",
+      "location": {
+        "address": "West velachery",
+        "postalCode": 600088,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/haridass-m-a7010a156/",
+          "github":"https://github.com/HaridassMurugan"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "R STAHL Private Limited (MNC)",
+        "position": "ASSOCIATE ENGINEER-DESIGN",
+        "startDate": "01-10-2020",
+        "endDate": "25-08-2022",
+        "summary": "i am the design engineer for design the safety explosion protection area electrical panels(i.e, electrical panel installed in ships, coal mining, on-shore and off-shore places,etc) handling various countries customers and gaining lots of experience to handle the customer and complete the project till reasched on customer place with guide of installation",
+      },
+    ],
+    "education": [
+      {
+        "institution": "KCG COLLEGE OF TECHNOLOGY",
+        "department": "ELECTRICAL AND ELECTRONICS ENGINEERING",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2018,
+        "gpa": 6.12,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Full stack developer",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "To create a innovative things, learn new things and browsing the wikipedia,",
+      }
+    ]
+  }
+  console.log(myResume);
